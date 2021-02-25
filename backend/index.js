0cb11b18d97f7a9039283a3038dc98884a81425f
Alisson <lucasali7@gmail.com>
2021-02-25 01:30:22 -0300

    [BackEnd] Using express-sslify

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const app = require('express')()
 const consign = require('consign')
 const db = require('./config/db')
+const enforce = require('express-sslify')
 
 app.db = db
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 consign()
     .include('./config/passport.js')
